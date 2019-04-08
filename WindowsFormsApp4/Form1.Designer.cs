d91df43106d64285463be650662e17b8022b8ebf
xepoman <ivan.egov@mail.ru>
2019-04-08 13:01:22 +0700

    init2

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -28,13 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.number1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.number2 = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.number6 = new System.Windows.Forms.Button();
+            this.number7 = new System.Windows.Forms.Button();
+            this.number8 = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.ravno = new System.Windows.Forms.Button();
+            this.dotButton = new System.Windows.Forms.Button();
+            this.umnogenieButton = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(2, 93);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(39, 40);
+            this.number1.TabIndex = 0;
+            this.number1.Text = "1";
+            this.number1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(47, 93);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(39, 40);
+            this.number2.TabIndex = 19;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = true;
+            // 
+            // number3
+            // 
+            this.number3.Location = new System.Drawing.Point(92, 93);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(39, 40);
+            this.number3.TabIndex = 20;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = true;
+            // 
+            // number4
+            // 
+            this.number4.Location = new System.Drawing.Point(2, 142);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(39, 40);
+            this.number4.TabIndex = 21;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = true;
+            // 
+            // number5
+            // 
+            this.number5.Location = new System.Drawing.Point(47, 142);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(39, 40);
+            this.number5.TabIndex = 22;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = true;
+            // 
+            // number6
+            // 
+            this.number6.Location = new System.Drawing.Point(92, 142);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(39, 40);
+            this.number6.TabIndex = 23;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = true;
+            // 
+            // number7
+            // 
+            this.number7.Location = new System.Drawing.Point(2, 188);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(39, 40);
+            this.number7.TabIndex = 24;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = true;
+            // 
+            // number8
+            // 
+            this.number8.Location = new System.Drawing.Point(47, 188);
+            this.number8.Name = "number8";
+            this.number8.Size = new System.Drawing.Size(39, 40);
+            this.number8.TabIndex = 25;
+            this.number8.Text = "8";
+            this.number8.UseVisualStyleBackColor = true;
+            // 
+            // number9
+            // 
+            this.number9.Location = new System.Drawing.Point(92, 188);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(39, 40);
+            this.number9.TabIndex = 26;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = true;
+            // 
+            // number0
+            // 
+            this.number0.Location = new System.Drawing.Point(47, 234);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(39, 40);
+            this.number0.TabIndex = 27;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(137, 93);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(39, 40);
+            this.minus.TabIndex = 28;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(137, 142);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(39, 40);
+            this.plus.TabIndex = 29;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(137, 188);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(39, 40);
+            this.Delete.TabIndex = 30;
+            this.Delete.Text = "/";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // ravno
+            // 
+            this.ravno.Location = new System.Drawing.Point(92, 234);
+            this.ravno.Name = "ravno";
+            this.ravno.Size = new System.Drawing.Size(39, 40);
+            this.ravno.TabIndex = 31;
+            this.ravno.Text = "=";
+            this.ravno.UseVisualStyleBackColor = true;
+            // 
+            // dotButton
+            // 
+            this.dotButton.Location = new System.Drawing.Point(2, 234);
+            this.dotButton.Name = "dotButton";
+            this.dotButton.Size = new System.Drawing.Size(39, 40);
+            this.dotButton.TabIndex = 32;
+            this.dotButton.Text = ".";
+            this.dotButton.UseVisualStyleBackColor = true;
+            // 
+            // umnogenieButton
+            // 
+            this.umnogenieButton.Location = new System.Drawing.Point(137, 234);
+            this.umnogenieButton.Name = "umnogenieButton";
+            this.umnogenieButton.Size = new System.Drawing.Size(39, 40);
+            this.umnogenieButton.TabIndex = 33;
+            this.umnogenieButton.Text = "*";
+            this.umnogenieButton.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(2, 51);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(39, 40);
+            this.btnClear.TabIndex = 34;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnCE
+            // 
+            this.btnCE.Location = new System.Drawing.Point(47, 51);
+            this.btnCE.Name = "btnCE";
+            this.btnCE.Size = new System.Drawing.Size(39, 40);
+            this.btnCE.TabIndex = 35;
+            this.btnCE.Text = "CE";
+            this.btnCE.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(187, 305);
+            this.Controls.Add(this.btnCE);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.umnogenieButton);
+            this.Controls.Add(this.dotButton);
+            this.Controls.Add(this.ravno);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.number0);
+            this.Controls.Add(this.number9);
+            this.Controls.Add(this.number8);
+            this.Controls.Add(this.number7);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.number1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button number1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button number2;
+        private System.Windows.Forms.Button number3;
+        private System.Windows.Forms.Button number4;
+        private System.Windows.Forms.Button number5;
+        private System.Windows.Forms.Button number6;
+        private System.Windows.Forms.Button number7;
+        private System.Windows.Forms.Button number8;
+        private System.Windows.Forms.Button number9;
+        private System.Windows.Forms.Button number0;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button ravno;
+        private System.Windows.Forms.Button dotButton;
+        private System.Windows.Forms.Button umnogenieButton;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCE;
     }
 }
 
